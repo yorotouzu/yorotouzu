47cb245f53c8f757b8e7ab2bff69f867065b28f2
yorotouzu <yorotouzu@admins-MacBook-Air.local>
2022-05-16 03:36:11 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'eslint-plugin-tsdoc', 'jsdoc'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-types': [ 'error', {
+      types: { null: 'Use undefined instead of null' },
+    }],
+    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+    'tsdoc/syntax': 'error',
+    'object-shorthand': ['error', 'always'],
+    'no-unreachable': 'error',
+    'jsdoc/require-jsdoc': [
+      'error',
+      {
+        contexts: ['MethodDefinition:not([accessibility="private"])'],
+        require: {
+          ClassDeclaration: true,
+        },
+        checkConstructors: false,
+        enableFixer: false,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['test/**/*', 'lib/**/*'],
+      rules: {
+        'jsdoc/require-jsdoc': 'off',
+        '@typescript-eslint/ban-types': 'off',
+      },
+    },
+  ],
+};
